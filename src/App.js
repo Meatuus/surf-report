3d97ba8fe7d64e8b843ddb68b813887a0c4626cb
Tyler <tcameronsmith@gmail.com>
2017-08-16 16:43:03 +1000

    remove logo from file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-// import React, { Component } from 'react';
-// import logo from './assets/logo.svg';
-// import './assets/css/App.css';
-// import conditions from './data/conditions';
-// import ConditionReport from './components/ConditionReport';
-
 import React from 'react';
 import {
   BrowserRouter as Router,
@@ -11,35 +5,9 @@ import {
   Link
 } from 'react-router-dom';
 import conditions from './data/conditions';
-import Home from './components/Home';
+import Home from './containers/Home';
 import ConditionReport from './components/ConditionReport';
-// import Experience from './Experience';
-// import Accommodation from './Accommodation';
 import './assets/css/App.css';
-import logo from './assets/logo.svg';
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <ConditionReport swellHeightMin={conditions.swell.minBreakingHeight}
-//           swellHeightMax={conditions.swell.maxBreakingHeight}
-//           swellUnit={conditions.swell.unit}
-//           swellDirection={conditions.swell.components.combined.compassDirection}
-//           windSpeed={conditions.wind.speed}
-//           windDirection={conditions.wind.compassDirection}
-//           windUnit={conditions.wind.unit}
-//           temperature={conditions.condition.temperature}
-//           temperatureUnit={conditions.condition.unit}
-//         />
-//       </div>
-//     );
-//   }
-// }
 
 const App = () => (
   <Router>
