8902df47b7888824fe499fbc180ef17028418d8a
Tyler <tcameronsmith@gmail.com>
2017-08-17 14:19:07 +1000

    add link to include Location component

--- src/components/ConditionReport.js
+++ src/components/ConditionReport.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import {
+  Route,
+  Link
+} from 'react-router-dom';
+import Location from '../containers/Location';
 
 const ConditionReport = props => (
-  <div className="App-intro">
-    <h2>{props.location}</h2>
-    <ul>
-      <li>
-        <h3>Swell</h3>
-        <div>{props.swellHeightMin} - {props.swellHeightMax} {props.swellUnit}</div>
-        <div>{props.swellDirection}</div>
-      </li>
-      <li>
-        <h3>Wind</h3>
-        <div>{props.windSpeed} {props.windUnit}</div>
-        <div>{props.windDirection}</div>
-      </li>
-      <li>
-        <h3>Temperature</h3>
-        <div>{props.temperature} {`${props.temperatureUnit}`.toUpperCase()}</div>
-      </li>
-    </ul>
+  <div>
+
+    <div>
+      <Link to={`/locations/${props.location}`}>
+        Test
+      </Link>
+
+      <Route path={`/locations/${props.location}`} component={Location} />
+    </div>
+    <div className="App-intro">
+      <h2>{props.location}</h2>
+      <ul>
+        <li>
+          <h3>Swell</h3>
+          <div>{props.swellHeightMin} - {props.swellHeightMax} {props.swellUnit}</div>
+          <div>{props.swellDirection}</div>
+        </li>
+        <li>
+          <h3>Wind</h3>
+          <div>{props.windSpeed} {props.windUnit}</div>
+          <div>{props.windDirection}</div>
+        </li>
+        <li>
+          <h3>Temperature</h3>
+          <div>{props.temperature} {`${props.temperatureUnit}`.toUpperCase()}</div>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
