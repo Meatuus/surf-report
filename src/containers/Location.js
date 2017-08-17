5e4233b2481d368b14ee048083e437da91eb6336
Tyler <tcameronsmith@gmail.com>
2017-08-17 14:17:40 +1000

    add location page - will include more details about specific locations

---
+++ src/containers/Location.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+// import HomeConditions from '../components/HomeConditions';
+
+const Location = props => (
+  <div>
+
+    <div className="App-intro">
+      {/* <h2>{props.location}</h2>
+        <ul>
+        <li>
+          <h3>Swell</h3>
+          <div>{props.swellHeightMin} - {props.swellHeightMax} {props.swellUnit}</div>
+          <div>{props.swellDirection}</div>
+        </li>
+        <li>
+          <h3>Wind</h3>
+          <div>{props.windSpeed} {props.windUnit}</div>
+          <div>{props.windDirection}</div>
+        </li>
+        <li>
+          <h3>Temperature</h3>
+          <div>{props.temperature} {`${props.temperatureUnit}`.toUpperCase()}</div>
+        </li>
+      </ul> */}
+      <h1>TEsting Location</h1>
+    </div>
+  </div>
+);
+
+export default Location;
