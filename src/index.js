94c587b305088d2ccf16b5d5f407729730768a52
Tyler <tcameronsmith@gmail.com>
2017-08-22 15:45:42 +1000

    pass each location api url to app

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './assets/css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App url="http://localhost:3001/api/surf" />, document.getElementById('root'));
+ReactDOM.render(
+  <App
+    urlFirst="http://localhost:3001/api/lennoxhead"
+    urlSecond="http://localhost:3001/api/ballina"
+    urlThird="http://localhost:3001/api/byronbay"
+  />,
+  document.getElementById('root'));
 registerServiceWorker();
