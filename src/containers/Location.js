1b9c3ce93755f2921f34f84821c2bcf9f9481f5c
Tyler <tcameronsmith@gmail.com>
2017-08-22 21:56:45 +1000

    iterate through array and map the 3 components to render for larger api data

--- src/containers/Location.js
+++ src/containers/Location.js
@@ -3,24 +3,55 @@ import LocationSwell from '../components/LocationSwell';
 import LocationWind from '../components/LocationWind';
 import LocationTemperature from '../components/LocationTemperature';
 
-const Location = (props) => (
-  <div className="App-intro">
-    <h1>{props.conditions.location}</h1>
-    <LocationSwell
-      swellHeightMin={props.conditions.swell.minBreakingHeight}
-      swellHeightMax={props.conditions.swell.maxBreakingHeight}
-      swellUnit={props.conditions.swell.unit}
-      swellDirection={props.conditions.swell.components.combined.compassDirection}
-    />
-    <LocationWind windSpeed={props.conditions.wind.speed}
-      windUnit={props.conditions.wind.unit}
-      windDirection={props.conditions.wind.compassDirection}
-    />
-    <LocationTemperature
-      temperature={props.conditions.condition.temperature}
-      temperatureUnit={props.conditions.condition.unit}
-    />
-  </div>
-);
+class Location extends Component {
+
+  render() {
+    const locationArray = this.props.conditions;
+    const locationEach = locationArray.map((item, index) => (
+      <div className="App-intro" key={index}>
+        <h1>{item.timestamp}</h1>
+        <LocationSwell
+          swellHeightMin={item.swell.minBreakingHeight}
+          swellHeightMax={item.swell.maxBreakingHeight}
+          swellUnit={item.swell.unit}
+          swellDirection={item.swell.components.combined.compassDirection}
+        />
+        <LocationWind windSpeed={item.wind.speed}
+          windUnit={item.wind.unit}
+          windDirection={item.wind.compassDirection}
+        />
+        <LocationTemperature
+          temperature={item.condition.temperature}
+          temperatureUnit={item.condition.unit}
+        />
+      </div>
+    ));
+
+    return(
+      <div>
+        {locationEach}
+      </div>
+    );
+  }
+}
+// const Location = (props) => (
+//   <div className="App-intro">
+//     <h1>{props.conditions.timestamp}</h1>
+//     <LocationSwell
+//       swellHeightMin={props.conditions.swell.minBreakingHeight}
+//       swellHeightMax={props.conditions.swell.maxBreakingHeight}
+//       swellUnit={props.conditions.swell.unit}
+//       swellDirection={props.conditions.swell.components.combined.compassDirection}
+//     />
+//     <LocationWind windSpeed={props.conditions.wind.speed}
+//       windUnit={props.conditions.wind.unit}
+//       windDirection={props.conditions.wind.compassDirection}
+//     />
+//     <LocationTemperature
+//       temperature={props.conditions.condition.temperature}
+//       temperatureUnit={props.conditions.condition.unit}
+//     />
+//   </div>
+// );
 
 export default Location;
