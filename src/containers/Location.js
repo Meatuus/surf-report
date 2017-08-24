a1f61fefe85a51e40a817f51dd5f72f47cf34f24
Tyler <tcameronsmith@gmail.com>
2017-08-24 15:26:24 +1000

    add day calculator for forecast section

--- src/containers/Location.js
+++ src/containers/Location.js
@@ -2,14 +2,82 @@ import React, {Component} from 'react';
 import LocationSwell from '../components/LocationSwell';
 import LocationWind from '../components/LocationWind';
 import LocationTemperature from '../components/LocationTemperature';
+import ConditionReport from '../components/ConditionReport';
 
 class Location extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentConditions: this.props.conditions,
+      conditionDays: []
+    }
+
+    this.getCurrent = this.getCurrent.bind(this);
+    this.getDays = this.getDays.bind(this);
+  }
+
+  getCurrent() {
+    let locationArray = [];
+    let date = (Date.now()/1000);
+
+    for (var i = 0; i < this.state.currentConditions.length; i++) {
+      if (this.state.currentConditions[i].timestamp > date) {
+        locationArray.push(this.props.conditions[i])
+      }
+    }
+
+    this.setState({
+      currentConditions: locationArray,
+    });
+  }
+
+  getDays() {
+    let days = []
+
+    for (var i = 0; i < this.props.conditions.length; i+=8) {
+      days.push(this.props.conditions[i])
+    }
+
+    this.setState({
+      conditionDays: days
+    })
+  }
+
+
+  componentDidMount() {
+    setInterval(this.getCurrent(), 5000);
+    this.getDays();
+  }
 
   render() {
+    const futureConditions = this.state.currentConditions
+    const futureConditionsArray = futureConditions.map((item, index) => (
+      <ConditionReport
+        swellHeightMin={item.swell.minBreakingHeight}
+        swellHeightMax={item.swell.maxBreakingHeight}
+        swellUnit={item.swell.unit}
+        swellDirection={item.swell.components.combined.compassDirection}
+        windSpeed={item.wind.speed}
+        windDirection={item.wind.compassDirection}
+        windUnit={item.wind.unit}
+        temperature={item.condition.temperature}
+        temperatureUnit={item.condition.unit}
+        key={index}
+      />
+    ))
+    const currentCondition = futureConditionsArray[0]
+
+    const weekdays = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    let days = this.state.conditionDays
+    const conditionDays = days.map((item, index) => (
+      <li><h2 key={index}>{weekdays[new Date(item.timestamp*1000).getDay()]}</h2></li>
+    ))
+
     const locationArray = this.props.conditions;
     const locationEach = locationArray.map((item, index) => (
       <div className="App-intro" key={index}>
-        <h1>{item.timestamp}</h1>
+        <h1>{`${new Date(item.timestamp*1000).getHours()}:${new Date(item.timestamp*1000).getMinutes()}0`}</h1>
         <LocationSwell
           swellHeightMin={item.swell.minBreakingHeight}
           swellHeightMax={item.swell.maxBreakingHeight}
@@ -29,29 +97,16 @@ class Location extends Component {
 
     return(
       <div>
+        <h1>Current Conditions</h1>
+        {currentCondition}
+        <h1>Forecast</h1>
+        <ul>
+          {conditionDays}
+        </ul>
         {locationEach}
       </div>
     );
   }
 }
-// const Location = (props) => (
-//   <div className="App-intro">
-//     <h1>{props.conditions.timestamp}</h1>
-//     <LocationSwell
-//       swellHeightMin={props.conditions.swell.minBreakingHeight}
-//       swellHeightMax={props.conditions.swell.maxBreakingHeight}
-//       swellUnit={props.conditions.swell.unit}
-//       swellDirection={props.conditions.swell.components.combined.compassDirection}
-//     />
-//     <LocationWind windSpeed={props.conditions.wind.speed}
-//       windUnit={props.conditions.wind.unit}
-//       windDirection={props.conditions.wind.compassDirection}
-//     />
-//     <LocationTemperature
-//       temperature={props.conditions.condition.temperature}
-//       temperatureUnit={props.conditions.condition.unit}
-//     />
-//   </div>
-// );
 
 export default Location;
