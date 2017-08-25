7869e1a3f282b7049b4cfc6ce152e67ac2f2a415
Tyler <tcameronsmith@gmail.com>
2017-08-25 15:35:27 +1000

    make Home component stateless

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,43 +2,29 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import CurrentConditions from '../components/CurrentConditions';
 
-class Home extends Component {
-  constructor(props) {
-    super(props)
-  //
-  //   this.state = {
-  //     // conditionsArray: this.props.conditions
-  //     locationsArray: this.props.locationFirst
-  //   }
-  }
-
-  render() {
-
-    return (
-      <div>
-        <h3>Home Page</h3>
-        <div>
-          <ul>
-            <Link to={`/location/${this.props.locationOneName.replace(/ /g,'_')}`}>Location: {this.props.locationOneName}</Link>{' '}
-            <Link to={`/location/${this.props.locationTwoName.replace(/ /g,'_')}`}>Location: {this.props.locationTwoName}</Link>{' '}
-            <Link to={`/location/${this.props.locationThreeName.replace(/ /g,'_')}`}>Location: {this.props.locationThreeName}</Link>
-          </ul>
-        </div>
-        <div>
-          <h1>{this.props.locationOneName}</h1>
-          <CurrentConditions conditions={this.props.locationOneConditions}/>
-        </div>
-        <div>
-          <h1>{this.props.locationTwoName}</h1>
-          <CurrentConditions conditions={this.props.locationTwoConditions}/>
-        </div>
-        <div>
-          <h1>{this.props.locationThreeName}</h1>
-          <CurrentConditions conditions={this.props.locationThreeConditions}/>
-        </div>
-      </div>
-    );
-  }
-}
+const Home = (props) => (
+  <div>
+    <h3>Home Page</h3>
+    <div>
+      <ul>
+        <Link to={`/location/${props.locationOneName.replace(/ /g,'_')}`}>Location: {props.locationOneName}</Link>{' '}
+        <Link to={`/location/${props.locationTwoName.replace(/ /g,'_')}`}>Location: {props.locationTwoName}</Link>{' '}
+        <Link to={`/location/${props.locationThreeName.replace(/ /g,'_')}`}>Location: {props.locationThreeName}</Link>
+      </ul>
+    </div>
+    <div>
+      <h1>{props.locationOneName}</h1>
+      <CurrentConditions conditions={props.locationOneConditions}/>
+    </div>
+    <div>
+      <h1>{props.locationTwoName}</h1>
+      <CurrentConditions conditions={props.locationTwoConditions}/>
+    </div>
+    <div>
+      <h1>{props.locationThreeName}</h1>
+      <CurrentConditions conditions={props.locationThreeConditions}/>
+    </div>
+  </div>
+);
 
 export default Home;
