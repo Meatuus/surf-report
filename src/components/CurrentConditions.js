5f1ece1bd991b8e8694fd8180924d0e8b52ed0f2
Tyler <tcameronsmith@gmail.com>
2017-08-25 15:28:18 +1000

    remove state from component, using props now only since this is static

--- src/components/CurrentConditions.js
+++ src/components/CurrentConditions.js
@@ -2,39 +2,46 @@ import React, {Component} from 'react';
 import ConditionReport from './ConditionReport';
 
 class CurrentConditions extends Component {
-  constructor(props) {
-    super(props)
+  // constructor(props) {
+  //   super(props)
+  //
+  //   // this.state = {
+  //   //   currentConditions: this.props.conditions
+  //   // }
+  //
+  //   // this.getCurrent = this.getCurrent.bind(this);
+  // }
 
-    this.state = {
-      currentConditions: this.props.conditions
-    }
+  // getCurrent() {
+  //   let locationArray = [];
+  //   let date = (Date.now()/1000);
+  //
+  //   for (var i = 0; i < this.state.currentConditions.length; i++) {
+  //     if (this.state.currentConditions[i].timestamp > date) {
+  //       locationArray.push(this.props.conditions[i])
+  //     }
+  //   }
+  //
+  //   this.setState({
+  //     currentConditions: locationArray,
+  //   });
+  // }
 
-    this.getCurrent = this.getCurrent.bind(this);
-  }
 
-  getCurrent() {
-    let locationArray = [];
+  // componentDidMount() {
+  //   setInterval(this.getCurrent(), 5000);
+  // }
+
+  render() {
     let date = (Date.now()/1000);
-
-    for (var i = 0; i < this.state.currentConditions.length; i++) {
-      if (this.state.currentConditions[i].timestamp > date) {
-        locationArray.push(this.props.conditions[i])
+    let futureConditionsArray = []
+    for (var i = 0; i < this.props.conditions.length; i++) {
+      if (this.props.conditions[i].timestamp > date) {
+        futureConditionsArray.push(this.props.conditions[i])
       }
     }
 
-    this.setState({
-      currentConditions: locationArray,
-    });
-  }
-
-
-  componentDidMount() {
-    setInterval(this.getCurrent(), 5000);
-  }
-
-  render() {
-    const futureConditions = this.state.currentConditions
-    const futureConditionsArray = futureConditions.map((item, index) => (
+    const futureConditions = futureConditionsArray.map((item, index) => (
       <ConditionReport
         time={item.timestamp}
         swellHeightMin={item.swell.minBreakingHeight}
@@ -48,8 +55,8 @@ class CurrentConditions extends Component {
         temperatureUnit={item.condition.unit}
         key={index}
       />
-    ))
-    const currentCondition = futureConditionsArray[0]
+    ));
+    const currentCondition = futureConditions[0]
 
     return(
       <div>
