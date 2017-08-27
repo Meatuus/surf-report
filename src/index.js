83983120b8f34dc415dbff9ba239e9e6615c7a27
Tyler <tcameronsmith@gmail.com>
2017-08-27 19:54:07 +1000

    pass names of location sto components

--- src/index.js
+++ src/index.js
@@ -6,8 +6,11 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <App
+    first="Shelly Beach"
     urlFirst="http://localhost:3001/api/shellybeach"
+    second="Ballina"
     urlSecond="http://localhost:3001/api/ballina"
+    third="Byron Bay"
     urlThird="http://localhost:3001/api/byronbay"
   />,
   document.getElementById('root'));
