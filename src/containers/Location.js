2d10a050bea7541a69f805a6d4478abdbcf02706
Tyler <tcameronsmith@gmail.com>
2017-08-28 14:28:03 +1000

    change text in button during click event

--- src/containers/Location.js
+++ src/containers/Location.js
@@ -1,8 +1,4 @@
 import React, {Component} from 'react';
-// import LocationSwell from '../components/LocationSwell';
-// import LocationWind from '../components/LocationWind';
-// import LocationTemperature from '../components/LocationTemperature';
-// import ConditionReport from '../components/ConditionReport';
 import CurrentConditions from '../components/CurrentConditions';
 import ForecastConditions from '../components/ForecastConditions';
 
@@ -29,26 +25,6 @@ class Location extends Component {
 
   render() {
 
-    // const locationArray = this.props.conditions;
-    // const locationEach = locationArray.map((item, index) => (
-    //   <div className="App-intro" key={index}>
-    //     <h1>{`${new Date(item.timestamp*1000).getHours()}:${new Date(item.timestamp*1000).getMinutes()}0`}</h1>
-    //     <LocationSwell
-    //       swellHeightMin={item.swell.minBreakingHeight}
-    //       swellHeightMax={item.swell.maxBreakingHeight}
-    //       swellUnit={item.swell.unit}
-    //       swellDirection={item.swell.components.combined.compassDirection}
-    //     />
-    //     <LocationWind windSpeed={item.wind.speed}
-    //       windUnit={item.wind.unit}
-    //       windDirection={item.wind.compassDirection}
-    //     />
-    //     <LocationTemperature
-    //       temperature={item.condition.temperature}
-    //       temperatureUnit={item.condition.unit}
-    //     />
-    //   </div>
-    // ));
     const { conditions, location } = this.props;
 
     const renderForecast = this.state.renderForecast;
