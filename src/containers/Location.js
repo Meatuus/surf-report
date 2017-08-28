e5ad5c9034328775ef91a87e786f964e5acbdec9
Tyler <tcameronsmith@gmail.com>
2017-08-28 10:06:44 +1000

    add button to render forecast and hide it onclick

--- src/containers/Location.js
+++ src/containers/Location.js
@@ -7,6 +7,25 @@ import CurrentConditions from '../components/CurrentConditions';
 import ForecastConditions from '../components/ForecastConditions';
 
 class Location extends Component {
+  constructor() {
+    super();
+
+    this.state = { renderForecast: false }
+
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  handleButtonClick() {
+    let state = (this.state.renderForecast === false ? true : false);
+
+    this.setState({renderForecast: state});
+
+    if (this.state.renderForecast === false) {
+      document.querySelector('.forecast_button').innerHTML = "Close Forecast";
+    } else {
+      document.querySelector('.forecast_button').innerHTML = "See 4 Day Forecast";
+    }
+  }
 
   render() {
 
@@ -32,11 +51,16 @@ class Location extends Component {
     // ));
     const { conditions, location } = this.props;
 
+    const renderForecast = this.state.renderForecast;
+
+    let button = (renderForecast === true ? <ForecastConditions conditions={conditions}/> : null)
+
     return(
       <div>
         <h2>{location}</h2>
         <CurrentConditions conditions={conditions}/>
-        <ForecastConditions conditions={conditions}/>
+        {button}
+        <button className="forecast_button" onClick={this.handleButtonClick}>See 4 Day Forecast</button>
       </div>
     );
   }
