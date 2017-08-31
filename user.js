bf5acd58b86c1210f2501d604882e513c139895b
Tyler <tcameronsmith@gmail.com>
2017-08-31 17:17:30 +1000

    add alert to table

--- user.js
+++ user.js
@@ -1,11 +1,12 @@
 const knex = require('knex')(require('./knexfile'))
 
 module.exports = {
-  createUser ({ username, password }) {
+  createUser ({ username, password, alert }) {
     console.log(`Add user ${username} with password ${password}`)
     return knex('user').insert({
       username,
-      password
+      password,
+      alert
     })
   }
 }
