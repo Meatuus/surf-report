39fc031933fecc72230a23391892075efa9a1a39
Tyler <tcameronsmith@gmail.com>
2017-09-04 17:18:05 +1000

    create functions to compare current conditions to users alert settings - send email if they match

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var request = require('request');
 
+var nodemailer = require('nodemailer');
+var {email, password} = require('./email');
+
 // var Surf = require('./model/surf');
 
 var app = express();
@@ -35,6 +38,9 @@ app.use(bodyParser.json());
 //   res.setHeader('Cache-Control', 'no-cache');
 //   next();
 // });
+const ballinaUrl = 'http://magicseaweed.com/api/f03a395d88e9766e7ba7b625c73cc794/forecast/?spot_id=698';
+const byronbayUrl = 'http://magicseaweed.com/api/f03a395d88e9766e7ba7b625c73cc794/forecast/?spot_id=541';
+const shellybeachUrl = 'http://magicseaweed.com/api/f03a395d88e9766e7ba7b625c73cc794/forecast/?spot_id=1036';
 
 app.use(cors());
 
@@ -44,7 +50,7 @@ router.get('/', function(req, res) {
 });
 
 app.get('/api/shellybeach', (req, res) => (
-  request('http://magicseaweed.com/api/f03a395d88e9766e7ba7b625c73cc794/forecast/?spot_id=1036', (error, response, body) => {
+  request(shellybeachUrl, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       var info = JSON.parse(body)
       res.send(info);
@@ -53,7 +59,7 @@ app.get('/api/shellybeach', (req, res) => (
 ))
 
 app.get('/api/ballina', (req, res) => (
-  request('http://magicseaweed.com/api/f03a395d88e9766e7ba7b625c73cc794/forecast/?spot_id=698', (error, response, body) => {
+  request(ballinaUrl, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       var info = JSON.parse(body)
       res.send(info);
@@ -62,7 +68,7 @@ app.get('/api/ballina', (req, res) => (
 ))
 
 app.get('/api/byronbay', (req, res) => (
-  request('http://magicseaweed.com/api/f03a395d88e9766e7ba7b625c73cc794/forecast/?spot_id=541', (error, response, body) => {
+  request(byronbayUrl, (error, response, body) => {
     if (!error && response.statusCode == 200) {
       var info = JSON.parse(body)
       res.send(info);
@@ -74,8 +80,12 @@ app.get('/api/users', (req, res) => {
   Users
     .fetchAll()
     .then(function(users) {
-        res.json({ users });
-      });
+      res.json({ users });
+
+      // pass array of all users to alert function => this will be moved to separate function
+      let array = users.models;
+      alerting(array);
+    })
 })
 
 app.get('/api/users/:username', (req, res) => {
@@ -87,6 +97,120 @@ app.get('/api/users/:username', (req, res) => {
     })
 })
 
+// TODO: have alerting function fire on interval of 3 hours instead of get request to users
+
+function alerting(array) {
+  const currentTime = (Date.now()/1000);
+  let currentConditionsBallina = [];
+  let currentConditionsByron = [];
+  let currentConditionsShelly = [];
+
+    for (var i = 0; i < array.length; i++) {
+      let alert = array[i].attributes.alert;
+      let location = array[i].attributes.location;
+      let minSwell = array[i].attributes.SwellMin;
+      let maxSwell = array[i].attributes.SwellMax;
+      let user = array[i].attributes.username
+
+      let mailOptions = {
+        from: emailFrom,
+        to: user,
+        subject: "Surf's up",
+        text: `it's ideal conditions at ${location}!!`
+      };
+
+      function sendEmail() {
+
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+          }
+        });
+      }
+
+      if (alert === 1 && location === "Ballina") {
+        request(ballinaUrl, (error, response, body) => {
+          if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body)
+
+            for (var i = 0; i < info.length; i++) {
+              if (info[i].timestamp > currentTime) {
+                currentConditionsBallina.push(info[i])
+              }
+            }
+            if (currentConditionsBallina[0].swell.minBreakingHeight === minSwell && currentConditionsBallina[0].swell.maxBreakingHeight === maxSwell) {
+              console.log('swells match');
+              sendEmail();
+            }
+          }
+        })
+      } else if (alert === 1 && location === "Byron Bay") {
+        request(byronbayUrl, (error, response, body) => {
+          if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body)
+
+            for (var i = 0; i < info.length; i++) {
+              if (info[i].timestamp > currentTime) {
+                currentConditionsByron.push(info[i])
+              }
+            }
+            if (currentConditionsByron[0].swell.minBreakingHeight === minSwell && currentConditionsByron[0].swell.maxBreakingHeight === maxSwell) {
+              console.log('swells match');
+              sendEmail();
+            }
+          }
+        })
+      } else if (alert === 1 && location === "Shelly Beach") {
+        request(shellybeachUrl, (error, response, body) => {
+          if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body)
+
+            for (var i = 0; i < info.length; i++) {
+              if (info[i].timestamp > currentTime) {
+                currentConditionsShelly.push(info[i])
+              }
+            }
+            if (currentConditionsShelly[0].swell.minBreakingHeight === minSwell && currentConditionsShelly[0].swell.maxBreakingHeight === maxSwell) {
+              console.log('swells match');
+              sendEmail();
+            }
+          }
+        })
+      }
+    }
+
+}
+
+function signupEmail(emailTo) {
+
+  let mailOptions = {
+    from: emailFrom,
+    to: emailTo,
+    subject: 'Welcome to my little surf alert app',
+    text: 'Hope you surf at one of our 3 locations!'
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: email,
+    pass: password
+  }
+});
+
+const emailFrom = email;
+
 app.post('/api/users', (req, res) => {
   user
     .createUser({
@@ -94,7 +218,11 @@ app.post('/api/users', (req, res) => {
       password: req.body.password,
       alert: req.body.alert
     })
-    .then(() => res.sendStatus(200))
+    .then(() => {
+      res.sendStatus(200)
+      console.log(req.body.username);
+      signupEmail(req.body.username);
+    })
 })
 
 app.put('/api/users/:username', (req, res) => {
