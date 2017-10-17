a981007c270bea9288c6feab96463b6339991b2f
Tyler <tcameronsmith@gmail.com>
2017-10-17 16:50:12 +1100

    update create user api call, now fill out all fields of db with default settings

--- user.js
+++ user.js
@@ -1,12 +1,16 @@
 const knex = require('knex')(require('./knexfile'))
 
 module.exports = {
-  createUser ({ username, password, alert }) {
+  createUser ({ username, password, alert, wind, location, SwellMin, SwellMax }) {
     console.log(`Add user ${username} with password ${password}`)
     return knex('user').insert({
       username,
       password,
-      alert
+      alert,
+      wind,
+      location,
+      SwellMin,
+      SwellMax
     })
   }
 }
